bfbe6e93bcfd4451454a549303789a547200d840
Utkarsh042 <singhutkarsh919@gmail.com>
2024-11-05 15:12:13 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
-import './App.css';
+import "./App.css";
 import SecurityCaptainForm from './SecurityCaptainForm';
 import FAQ from './FAQ';
+import React from "react";
+import BenefitsSection from "./Components/BenefitsSection";
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     
     <>
     <SecurityCaptainForm></SecurityCaptainForm>
-    
+    <div className="App">
+    <BenefitsSection />
+    </div>
     <FAQ></FAQ>
+    <div className="App">
+      <Footer />
+    </div>
     </>
   );
 }
